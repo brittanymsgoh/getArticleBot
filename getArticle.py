a5683199422a48e6cdaf8e5df2f85ef3bff19701
Brittany <brittanyms.goh@gmail.com>
2020-10-30 13:04:39 +0800

    Update getArticle.py

--- getArticle.py
+++ getArticle.py
@@ -4,8 +4,6 @@ import requests
 API_TOKEN = '<TOKEN>'
 bot = TeleBot(API_TOKEN)
 
-# COOKIES_FINAL = dict(__cfduid='d58ea26e584d007fac1934a884ae501b41592678834', uid='lo_7fAnCjekKaUe', sid='1:apIzsi6ptxOIGtDCSkhoSJL8XJYQep4+o9G4PbrD7uW2umoFLkn3LLZ47uHXavQw', optimizelyEndUserId='lo_7fAnCjekKaUe', __cfruid='bd43f6b7631d1c48d2359ced750fe4b59f32b4d4-1592678834')
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Hello! Click /getArticle to get started on getting your free Medium Articles!')
